5b04b5111bedc9dd322bce8eadcdba49ee33cc75
Nicolas Hefti <nicolas@hefti.fr>
2016-02-03 16:45:10 +0100

    Fix bootstrap

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,11 +4,12 @@ define('ABS_PATH', dirname(__FILE__) . '/..');
 
 if (file_exists(ABS_PATH . '/app-config.php')) {
 
-    require_once(ABS_PATH . '/app-config-test.php');
+    require_once(ABS_PATH . '/app-config.php');
 
 } elseif (file_exists(ABS_PATH . '/app-config-sample.php')) {
 
     require_once(ABS_PATH . '/app-config-sample.php');
+
 } else {
 
     die('Did not find any config file.');
